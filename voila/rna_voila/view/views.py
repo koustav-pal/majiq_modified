7a89ce984f893d8cc18694cb9e141cf3510ac779
sjewell <sjewell@biociphers.org>
2024-07-10 15:20:57 -0400

    -fixed a sneaky bug which caused the splicegraph reads values to be duplicated for deltapsi majiq-SPEL outputs.

--- voila/rna_voila/view/views.py
+++ voila/rna_voila/view/views.py
@@ -196,6 +196,7 @@ def copy_lsv(lsv_id, view_matrix, voila_file=None):
                 'exons': exons
             }
             for junc in juncs:
+                junc = junc.copy()
                 junction_reads = list(sg.junction_reads_exp(junc, exp_names))
                 reads = sum(r['reads'] for r in junction_reads)
                 junc['reads'] = reads
