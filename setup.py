d930d5804c2386fac3708b329604b6a0a185d110
sjewell <sjewell@biociphers.org>
2024-07-24 15:06:49 -0400

    -re-added setup.py method after verifying toml method was ill-advised for this type of setup process.

--- setup.py
+++ setup.py
@@ -1,12 +1,13 @@
-from pathlib import Path
 from setuptools import setup
+from pathlib import Path
 
-local_path = Path(__file__).resolve().parent
+repo_path = Path(__file__).resolve().parent
 
 setup(
+    packages=[],
     install_requires=[
-        f"rna_moccasin @ {local_path}/moccasin",
-        f"rna_majiq @ {local_path}/majiq",
-        f"rna_voila @ {local_path}/voila",
-    ]
+        f"rna_moccasin @ file://localhost{repo_path}/moccasin/",
+        f"rna_majiq @ file://localhost{repo_path}/majiq/",
+        f"rna_voila @ file://localhost{repo_path}/voila/",
+    ],
 )
