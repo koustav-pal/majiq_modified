82832efd313a005259c58a9b55816351debcdc96
sjewell <sjewell@biociphers.org>
2025-07-22 13:58:54 -0400

    Incremented release version to 3.0.5

--- voila/setup.py
+++ voila/setup.py
@@ -21,6 +21,6 @@ setup(
     use_scm_version={
         "root": "..",
         "relative_to": __file__,
-        "fallback_version": "3.0.4+scmfallback",
+        "fallback_version": "3.0.5+scmfallback",
     },
 )
